92b6105120e48c6b1f25a8aba1e4f6aaf86351e1
HaJincheol <smilecjf@naver.com>
2024-04-26 06:22:26 +0000

    Revise AES evaluation

--- src/aes_he.rs
+++ src/aes_he.rs
@@ -27,47 +27,9 @@ pub fn he_add_round_key<Scalar, StateCont, RkCont>(
     lwe_ciphertext_list_add_assign(he_state, he_round_key.as_view());
 }
 
-pub fn he_sub_bytes_by_pksk<Scalar, KskCont>(
-    he_state: &mut LweCiphertextListOwned<Scalar>,
-    ksk: &LweKeyswitchKey<KskCont>,
-    fourier_bsk: FourierLweBootstrapKeyView,
-    pksk: &LwePackingKeyswitchKeyView<Scalar>,
-    ss_key: FourierGgswCiphertextListView,
-    ggsw_base_log: DecompositionBaseLog,
-    ggsw_level: DecompositionLevelCount,
-    log_lut_count: LutCountLog,
-) where
-    Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>,
-    KskCont: Container<Element=Scalar>,
-{
-    let buf = he_state.clone();
-    *he_state = LweCiphertextList::new(
-        Scalar::ZERO,
-        fourier_bsk.output_lwe_dimension().to_lwe_size(),
-        LweCiphertextCount(BLOCKSIZE_IN_BIT),
-        he_state.ciphertext_modulus(),
-    );
-
-    for (input_byte, mut output_byte) in buf.chunks_exact(BYTESIZE)
-        .zip(he_state.chunks_exact_mut(BYTESIZE))
-    {
-        he_sbox_eval_by_pksk(
-            &input_byte,
-            &mut output_byte,
-            ksk,
-            fourier_bsk,
-            pksk,
-            ss_key,
-            ggsw_base_log,
-            ggsw_level,
-            log_lut_count,
-        );
-    }
-}
-
-pub fn he_sub_bytes_by_trace_with_preprocessing<Scalar, KskCont>(
-    he_state: &mut LweCiphertextListOwned<Scalar>,
-    ksk: &LweKeyswitchKey<KskCont>,
+pub fn he_sub_bytes_by_patched_wwllp_cbs<Scalar, InputCont, OutputCont>(
+    he_state_input: &LweCiphertextList<InputCont>,
+    he_state_output: &mut LweCiphertextList<OutputCont>,
     fourier_bsk: FourierLweBootstrapKeyView,
     auto_keys: &HashMap<usize, AutomorphKey<ABox<[c64]>>>,
     ss_key: FourierGgswCiphertextListView,
@@ -76,23 +38,15 @@ pub fn he_sub_bytes_by_trace_with_preprocessing<Scalar, KskCont>(
     log_lut_count: LutCountLog,
 ) where
     Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>,
-    KskCont: Container<Element=Scalar>,
+    InputCont: Container<Element = Scalar>,
+    OutputCont: ContainerMut<Element = Scalar>,
 {
-    let buf = he_state.clone();
-    *he_state = LweCiphertextList::new(
-        Scalar::ZERO,
-        fourier_bsk.output_lwe_dimension().to_lwe_size(),
-        LweCiphertextCount(BLOCKSIZE_IN_BIT),
-        he_state.ciphertext_modulus(),
-    );
-
-    for (input_byte, mut output_byte) in buf.chunks_exact(BYTESIZE)
-        .zip(he_state.chunks_exact_mut(BYTESIZE))
+    for (input_byte, mut output_byte) in he_state_input.chunks_exact(BYTESIZE)
+        .zip(he_state_output.chunks_exact_mut(BYTESIZE))
     {
-        he_sbox_eval_by_trace_with_preprocessing(
+        he_sbox_eval_by_patched_wwllp_cbs(
             &input_byte,
             &mut output_byte,
-            ksk,
             fourier_bsk,
             auto_keys,
             ss_key,
@@ -225,122 +179,9 @@ where
     he_byte.as_mut().clone_from_slice(buf.as_ref());
 }
 
-fn he_sbox_eval_by_pksk<Scalar, InCont, OutCont, KskCont>(
+fn he_sbox_eval_by_patched_wwllp_cbs<Scalar, InCont, OutCont>(
     input: &LweCiphertextList<InCont>,
     output: &mut LweCiphertextList<OutCont>,
-    ksk: &LweKeyswitchKey<KskCont>,
-    fourier_bsk: FourierLweBootstrapKeyView,
-    pksk: &LwePackingKeyswitchKeyView<Scalar>,
-    ss_key: FourierGgswCiphertextListView,
-    ggsw_base_log: DecompositionBaseLog,
-    ggsw_level: DecompositionLevelCount,
-    log_lut_count: LutCountLog,
-) where
-    Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>,
-    InCont: Container<Element=Scalar>,
-    OutCont: ContainerMut<Element=Scalar>,
-    KskCont: Container<Element=Scalar>,
-{
-    let glwe_size = fourier_bsk.glwe_size();
-    let polynomial_size = fourier_bsk.polynomial_size();
-    let ciphertext_modulus = output.ciphertext_modulus();
-
-    let mut vec_glev = vec![
-        GlweCiphertextList::new(
-            Scalar::ZERO,
-            glwe_size,
-            polynomial_size,
-            GlweCiphertextCount(ggsw_level.0),
-            ciphertext_modulus,
-        ); BYTESIZE];
-    for (input_bit, glev) in input.iter().zip(vec_glev.iter_mut()) {
-        let mut input_bit_ks = LweCiphertext::new(
-            Scalar::ZERO,
-            ksk.output_lwe_size(),
-            ciphertext_modulus,
-        );
-        keyswitch_lwe_ciphertext(ksk, &input_bit, &mut input_bit_ks);
-        let glev_mut_view = GlweCiphertextListMutView::from_container(
-            glev.as_mut(),
-            glwe_size,
-            polynomial_size,
-            ciphertext_modulus,
-        );
-        lwe_msb_bit_to_glev_by_pksk(
-            input_bit_ks.as_view(),
-            glev_mut_view,
-            fourier_bsk,
-            pksk,
-            ggsw_base_log,
-            ggsw_level,
-            log_lut_count,
-        );
-    }
-
-    let mut ggsw_bit_list = GgswCiphertextList::new(
-        Scalar::ZERO,
-        glwe_size,
-        polynomial_size,
-        ggsw_base_log,
-        ggsw_level,
-        GgswCiphertextCount(vec_glev.len()),
-        ciphertext_modulus,
-    );
-    for (mut ggsw, glev) in ggsw_bit_list.iter_mut().zip(vec_glev.iter()) {
-        switch_scheme(&glev, &mut ggsw, ss_key);
-    }
-
-    let mut fourier_ggsw_bit_list = FourierGgswCiphertextList::new(
-        vec![c64::default();
-        BYTESIZE * polynomial_size.to_fourier_polynomial_size().0
-            * glwe_size.0
-            * glwe_size.0
-            * ggsw_level.0
-        ],
-        BYTESIZE,
-        glwe_size,
-        polynomial_size,
-        ggsw_base_log,
-        ggsw_level,
-    );
-    for (mut fourier_ggsw, ggsw) in fourier_ggsw_bit_list.as_mut_view().into_ggsw_iter().zip(ggsw_bit_list.iter()) {
-        convert_standard_ggsw_ciphertext_to_fourier(&ggsw, &mut fourier_ggsw);
-    }
-
-    let num_par_lut = polynomial_size.0 / (1 << BYTESIZE);
-    let num_accumulator = if BYTESIZE % num_par_lut == 0 {
-        BYTESIZE / num_par_lut
-    } else {
-        BYTESIZE / num_par_lut + 1
-    };
-
-    for acc_idx in 0..num_accumulator {
-        let accumulator = (0..polynomial_size.0).map(|i| {
-            let lut_idx = acc_idx * num_par_lut + i / (1 << BYTESIZE);
-            (((AES128_SBOX[i % (1 << BYTESIZE)] & (1 << lut_idx)) as usize) << ((Scalar::BITS - 1) - lut_idx)).cast_into()
-        }).collect::<Vec<Scalar>>();
-        let accumulator_plaintext = PlaintextList::from_container(accumulator);
-        let mut accumulator = allocate_and_trivially_encrypt_new_glwe_ciphertext(glwe_size, &accumulator_plaintext, ciphertext_modulus);
-
-        for (i, fourier_ggsw_bit) in fourier_ggsw_bit_list.as_view().into_ggsw_iter().into_iter().enumerate() {
-            let mut buf = accumulator.clone();
-            glwe_ciphertext_monic_monomial_div_assign(&mut buf, MonomialDegree(1 << i));
-            glwe_ciphertext_sub_assign(&mut buf, &accumulator);
-            add_external_product_assign(&mut accumulator, &fourier_ggsw_bit, &buf);
-        }
-
-        for i in 0..num_par_lut {
-            let bit_idx = acc_idx * num_par_lut + i;
-            let mut lwe_out = output.get_mut(bit_idx);
-            extract_lwe_sample_from_glwe_ciphertext(&accumulator, &mut lwe_out, MonomialDegree(i * (1 << BYTESIZE)));
-        }
-    }
-}
-
-fn he_sbox_eval_by_trace_with_preprocessing<Scalar, InCont, OutCont, KskCont>(
-    input: &LweCiphertextList<InCont>,
-    output: &mut LweCiphertextList<OutCont>,
-    ksk: &LweKeyswitchKey<KskCont>,
     fourier_bsk: FourierLweBootstrapKeyView,
     auto_keys: &HashMap<usize, AutomorphKey<ABox<[c64]>>>,
     ss_key: FourierGgswCiphertextListView,
@@ -351,7 +192,6 @@ fn he_sbox_eval_by_trace_with_preprocessing<Scalar, InCont, OutCont, KskCont>(
     Scalar: UnsignedTorus + CastInto<usize> + CastFrom<usize>,
     InCont: Container<Element=Scalar>,
     OutCont: ContainerMut<Element=Scalar>,
-    KskCont: Container<Element=Scalar>,
 {
     let glwe_size = fourier_bsk.glwe_size();
     let polynomial_size = fourier_bsk.polynomial_size();
@@ -373,14 +213,8 @@ fn he_sbox_eval_by_trace_with_preprocessing<Scalar, InCont, OutCont, KskCont>(
             ciphertext_modulus,
         );
 
-        let mut input_bit_ks = LweCiphertext::new(
-            Scalar::ZERO,
-            ksk.output_lwe_size(),
-            ciphertext_modulus,
-        );
-        keyswitch_lwe_ciphertext(ksk, &input_bit, &mut input_bit_ks);
         lwe_msb_bit_to_glev_by_trace_with_preprocessing(
-            input_bit_ks.as_view(),
+            input_bit.as_view(),
             glev_mut_view,
             fourier_bsk,
             auto_keys,
--- tests/aes_eval.rs
+++ tests/aes_eval.rs
@@ -5,28 +5,29 @@ use tfhe::core_crypto::prelude::*;
 use hom_trace::{keygen_pbs, aes_he::*, aes_ref::*, automorphism::*, ggsw_conv::*, FftType};
 
 fn main() {
-    // AES evaluation by trace with preprocessing
-    let lwe_dimension = LweDimension(742);
-    let lwe_modular_std_dev = StandardDev(0.000007069849454709433);
-    let polynomial_size = PolynomialSize(2048);
-    let glwe_dimension = GlweDimension(1);
+    // AES evaluation by patched WWLL+ circuit bootstrapping
+    let lwe_dimension = LweDimension(769);
+    let lwe_modular_std_dev = StandardDev(0.0000043131554647504185);
+    let polynomial_size = PolynomialSize(1024);
+    let glwe_dimension = GlweDimension(2);
     let glwe_modular_std_dev = StandardDev(0.00000000000000029403601535432533);
-    let pbs_base_log = DecompositionBaseLog(23);
-    let pbs_level = DecompositionLevelCount(1);
-    let ks_level = DecompositionLevelCount(5);
-    let ks_base_log = DecompositionBaseLog(3);
+
+    let pbs_base_log = DecompositionBaseLog(15);
+    let pbs_level = DecompositionLevelCount(2);
+    let ks_level = DecompositionLevelCount(2);
+    let ks_base_log = DecompositionBaseLog(6);
     let ciphertext_modulus = CiphertextModulus::<u64>::new_native();
 
     let ggsw_base_log = DecompositionBaseLog(5);
-    let ggsw_level = DecompositionLevelCount(4);
-    let auto_base_log = DecompositionBaseLog(12);
-    let auto_level = DecompositionLevelCount(3);
-    let fft_type = FftType::Vanilla;
-    let ss_base_log = DecompositionBaseLog(9);
-    let ss_level = DecompositionLevelCount(4);
+    let ggsw_level = DecompositionLevelCount(3);
+    let auto_base_log = DecompositionBaseLog(7);
+    let auto_level = DecompositionLevelCount(7);
+    let fft_type = FftType::Split16;
+    let ss_base_log = DecompositionBaseLog(8);
+    let ss_level = DecompositionLevelCount(6);
     let log_lut_count = LutCountLog(2);
 
-    test_aes_eval_by_trace_with_preprocessing(
+    test_aes_eval_by_patched_wwllp_cbs(
         lwe_dimension,
         polynomial_size,
         glwe_dimension,
@@ -45,37 +46,10 @@ fn main() {
         log_lut_count,
         ciphertext_modulus,
     );
-    println!();
-    println!();
-
-    // AES evaluation by pksk
-    let pksk_base_log = DecompositionBaseLog(12);
-    let pksk_level = DecompositionLevelCount(2);
-
-    test_aes_eval_by_pksk(
-        lwe_dimension,
-        polynomial_size,
-        glwe_dimension,
-        lwe_modular_std_dev,
-        glwe_modular_std_dev,
-        pbs_base_log, pbs_level,
-        ks_base_log,
-        ks_level,
-        ss_base_log,
-        ss_level,
-        pksk_base_log,
-        pksk_level,
-        ggsw_base_log,
-        ggsw_level,
-        log_lut_count,
-        ciphertext_modulus,
-    );
-    println!();
-    println!();
 }
 
 #[allow(unused)]
-fn test_aes_eval_by_trace_with_preprocessing(
+fn test_aes_eval_by_patched_wwllp_cbs(
     lwe_dimension: LweDimension,
     polynomial_size: PolynomialSize,
     glwe_dimension: GlweDimension,
@@ -96,7 +70,7 @@ fn test_aes_eval_by_trace_with_preprocessing(
     ciphertext_modulus: CiphertextModulus::<u64>,
 ) {
     println!(
-"==== AES evaluation by trace with preprocessing ====
+"==== AES evaluation by patched WWLL+ circuit bootstrapping ====
 n: {}, N: {}, k: {}, l_ks: {}, B_ks: 2^{}
 l_pbs: {}, B_pbs: 2^{}, l_ggsw: {}, B_ggsw: 2^{}, LutCount: 2^{},
 l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
@@ -116,7 +90,7 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
     let (
         lwe_sk,
         glwe_sk,
-        _lwe_sk_after_ks,
+        lwe_sk_after_ks,
         fourier_bsk,
         ksk,
     ) = keygen_pbs(
@@ -173,11 +147,11 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
     let num_bytes_to_print = 2;
     let mut he_round_keys = Vec::<LweCiphertextListOwned<u64>>::with_capacity(NUM_ROUNDS + 1);
     for r in 0..=NUM_ROUNDS {
-        let lwe_size = if r < NUM_ROUNDS {fourier_bsk.output_lwe_dimension().to_lwe_size()} else {fourier_bsk.output_lwe_dimension().to_lwe_size()};
+        // let lwe_size = if r < NUM_ROUNDS {fourier_bsk.output_lwe_dimension().to_lwe_size()} else {fourier_bsk.output_lwe_dimension().to_lwe_size()};
 
         let mut lwe_list_rk = LweCiphertextList::new(
             0u64,
-            lwe_size,
+            fourier_bsk.output_lwe_dimension().to_lwe_size(),
             LweCiphertextCount(BLOCKSIZE_IN_BIT),
             ciphertext_modulus,
         );
@@ -206,12 +180,20 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
         LweCiphertextCount(BLOCKSIZE_IN_BIT),
         ciphertext_modulus,
     );
+    let mut he_state_ks = LweCiphertextList::new(
+        0u64,
+        ksk.output_lwe_size(),
+        LweCiphertextCount(BLOCKSIZE_IN_BIT),
+        ciphertext_modulus,
+    );
+
     for (bit_idx, mut he_bit) in he_state.iter_mut().enumerate() {
         let byte_idx = bit_idx / 8;
         let pt = (message[byte_idx] & (1 << bit_idx)) >> bit_idx;
         *he_bit.get_mut_body().data += (pt as u64) << 63;
     }
 
+    let mut time_lwe_ks = Duration::ZERO;
     let mut time_sub_bytes = Duration::ZERO;
     let mut time_linear = Duration::ZERO;
 
@@ -225,11 +207,25 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
 
     for r in 1..NUM_ROUNDS {
         println!("Round {r}");
+        // LWE KS
+        let now = Instant::now();
+        for (lwe, mut lwe_ks) in he_state.iter().zip(he_state_ks.iter_mut()) {
+            keyswitch_lwe_ciphertext(&ksk, &lwe, &mut lwe_ks);
+        }
+        time_lwe_ks += now.elapsed();
+
+        let (vec_err, max_err) = get_he_state_error(&he_state_ks, state, &lwe_sk_after_ks);
+        print!("  - LWE ks  :");
+        for bit_err in vec_err.iter().take(BYTESIZE * num_bytes_to_print) {
+            print!(" {bit_err:>2}");
+        }
+        println!(" ... (max: {:.3})", (max_err as f64).log2());
+
         // SubBytes
         let now = Instant::now();
-        he_sub_bytes_by_trace_with_preprocessing(
+        he_sub_bytes_by_patched_wwllp_cbs(
+            &he_state_ks,
             &mut he_state,
-            &ksk,
             fourier_bsk,
             &auto_keys,
             ss_key,
@@ -274,9 +270,15 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
     println!("Final Round");
     // SubBytes
     let now = Instant::now();
-    he_sub_bytes_by_trace_with_preprocessing(
+    for (lwe, mut lwe_ks) in he_state.iter().zip(he_state_ks.iter_mut()) {
+        keyswitch_lwe_ciphertext(&ksk, &lwe, &mut lwe_ks);
+    }
+    time_lwe_ks += now.elapsed();
+
+    let now = Instant::now();
+    he_sub_bytes_by_patched_wwllp_cbs(
+        &he_state_ks,
         &mut he_state,
-        &ksk,
         fourier_bsk,
         &auto_keys,
         ss_key,
@@ -314,257 +316,10 @@ l_auto: {}, B_auto: 2^{}, l_ss: {}, B_ss: 2^{}\n",
 
     // Evaluation Time
     println!("\n---- Evaluation Time ----");
+    println!("LWE KS  : {} s", time_lwe_ks.as_millis() as f64 / 1000f64);
     println!("SubBytes: {} s", time_sub_bytes.as_millis() as f64 / 1000f64);
     println!("Linear  : {} ms", time_linear.as_micros() as f64 / 1000f64);
 
-    let time_total = time_sub_bytes + time_linear;
-    println!("Total   : {} s", time_total.as_millis() as f64 / 1000f64);
-}
-
-#[allow(unused)]
-fn test_aes_eval_by_pksk(
-    lwe_dimension: LweDimension,
-    polynomial_size: PolynomialSize,
-    glwe_dimension: GlweDimension,
-    lwe_modular_std_dev: impl DispersionParameter,
-    glwe_modular_std_dev: impl DispersionParameter,
-    pbs_base_log: DecompositionBaseLog,
-    pbs_level: DecompositionLevelCount,
-    ks_base_log: DecompositionBaseLog,
-    ks_level: DecompositionLevelCount,
-    ss_base_log: DecompositionBaseLog,
-    ss_level: DecompositionLevelCount,
-    pksk_base_log: DecompositionBaseLog,
-    pksk_level: DecompositionLevelCount,
-    ggsw_base_log: DecompositionBaseLog,
-    ggsw_level: DecompositionLevelCount,
-    log_lut_count: LutCountLog,
-    ciphertext_modulus: CiphertextModulus::<u64>,
-) {
-    println!(
-"==== AES evaluation by pksk ====
-n: {}, N: {}, k: {}, l_ks: {}, B_ks: 2^{}
-l_pbs: {}, B_pbs: 2^{}, l_ggsw: {}, B_ggsw: 2^{}, LutCount: 2^{},
-l_pksk: {}, B_pksk: 2^{}, l_ss: {}, B_ss: 2^{}\n",
-        lwe_dimension.0, polynomial_size.0, glwe_dimension.0, ks_level.0, ks_base_log.0,
-        pbs_level.0, pbs_base_log.0, ggsw_level.0, ggsw_base_log.0, log_lut_count.0,
-        pksk_level.0, pksk_base_log.0, ss_level.0, ss_base_log.0,
-    );
-
-    // Set random generators and buffers
-    let mut boxed_seeder = new_seeder();
-    let seeder = boxed_seeder.as_mut();
-
-    let mut secret_generator = SecretRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed());
-    let mut encryption_generator = EncryptionRandomGenerator::<ActivatedRandomGenerator>::new(seeder.seed(), seeder);
-
-    // Generate keys
-    let (
-        lwe_sk,
-        glwe_sk,
-        _lwe_sk_after_ks,
-        fourier_bsk,
-        ksk,
-    ) = keygen_pbs(
-        lwe_dimension,
-        glwe_dimension,
-        polynomial_size,
-        lwe_modular_std_dev,
-        glwe_modular_std_dev,
-        pbs_base_log,
-        pbs_level,
-        ks_base_log,
-        ks_level,
-        &mut secret_generator,
-        &mut encryption_generator,
-    );
-    let fourier_bsk = fourier_bsk.as_view();
-
-    let ss_key = generate_scheme_switching_key(
-        &glwe_sk,
-        ss_base_log,
-        ss_level,
-        glwe_modular_std_dev,
-        ciphertext_modulus,
-        &mut encryption_generator,
-    );
-    let ss_key = ss_key.as_view();
-
-    let pksk = allocate_and_generate_new_lwe_packing_keyswitch_key(
-        &lwe_sk,
-        &glwe_sk,
-        pksk_base_log,
-        pksk_level,
-        glwe_modular_std_dev,
-        ciphertext_modulus,
-        &mut encryption_generator,
-    );
-    let pksk = pksk.as_view();
-
-    // ======== Plain ========
-    let mut rng = rand::thread_rng();
-    let mut key = [0u8; BLOCKSIZE_IN_BYTE];
-    for i in 0..BLOCKSIZE_IN_BYTE {
-        key[i] = rng.gen_range(0..=u8::MAX);
-    }
-
-    let aes = Aes128Ref::new(&key);
-    let round_keys = aes.get_round_keys();
-
-    let mut message = [0u8; BLOCKSIZE_IN_BYTE];
-    for i in 0..16 {
-        message[i] = rng.gen_range(0..=255);
-    }
-    let mut state = byte_array_to_mat(message);
-
-    // ======== HE ========
-    let num_bytes_to_print = 2;
-    let mut he_round_keys = Vec::<LweCiphertextListOwned<u64>>::with_capacity(NUM_ROUNDS + 1);
-    for r in 0..=NUM_ROUNDS {
-        let lwe_size = if r < NUM_ROUNDS {fourier_bsk.output_lwe_dimension().to_lwe_size()} else {fourier_bsk.output_lwe_dimension().to_lwe_size()};
-
-        let mut lwe_list_rk = LweCiphertextList::new(
-            0u64,
-            lwe_size,
-            LweCiphertextCount(BLOCKSIZE_IN_BIT),
-            ciphertext_modulus,
-        );
-
-        let rk = PlaintextList::from_container((0..BLOCKSIZE_IN_BIT).map(|i| {
-            let byte_idx = i / BYTESIZE;
-            let bit_idx = i % BYTESIZE;
-            let round_key_byte = round_keys[r][byte_idx];
-            let round_key_bit = (round_key_byte & (1 << bit_idx)) >> bit_idx;
-            (round_key_bit as u64) << 63
-        }).collect::<Vec<u64>>());
-        encrypt_lwe_ciphertext_list(
-            &lwe_sk,
-            &mut lwe_list_rk,
-            &rk,
-            glwe_modular_std_dev,
-            &mut encryption_generator,
-        );
-
-        he_round_keys.push(lwe_list_rk);
-    }
-
-    let mut he_state = LweCiphertextList::new(
-        0u64,
-        fourier_bsk.output_lwe_dimension().to_lwe_size(),
-        LweCiphertextCount(BLOCKSIZE_IN_BIT),
-        ciphertext_modulus,
-    );
-    for (bit_idx, mut he_bit) in he_state.iter_mut().enumerate() {
-        let byte_idx = bit_idx / 8;
-        let pt = (message[byte_idx] & (1 << bit_idx)) >> bit_idx;
-        *he_bit.get_mut_body().data += (pt as u64) << 63;
-    }
-
-    let mut time_sub_bytes = Duration::ZERO;
-    let mut time_linear = Duration::ZERO;
-
-    println!("---- Error (bits) ----");
-    // AddRoundKey
-    let now = Instant::now();
-    he_add_round_key(&mut he_state, &he_round_keys[0]);
-    time_linear += now.elapsed();
-
-    aes.add_round_key(&mut state, 0);
-
-    for r in 1..NUM_ROUNDS {
-        println!("Round {r}");
-        // SubBytes
-        let now = Instant::now();
-        he_sub_bytes_by_pksk(
-            &mut he_state,
-            &ksk,
-            fourier_bsk,
-            &pksk,
-            ss_key,
-            ggsw_base_log,
-            ggsw_level,
-            log_lut_count,
-        );
-        time_sub_bytes += now.elapsed();
-
-        aes.sub_bytes(&mut state);
-        let (vec_err, max_err) = get_he_state_error(&he_state, state, &lwe_sk);
-        print!("  - SubBytes:");
-        for bit_err in vec_err.iter().take(BYTESIZE * num_bytes_to_print) {
-            print!(" {bit_err:>2}");
-        }
-        println!(" ... (max: {:.3})", (max_err as f64).log2());
-
-        let now = Instant::now();
-        // ShiftRows
-        he_shift_rows(&mut he_state);
-
-        // MixColumns
-        he_mix_columns(&mut he_state);
-
-        // AddRoundKey
-        he_add_round_key(&mut he_state, &he_round_keys[r]);
-        time_linear += now.elapsed();
-
-        aes.shift_rows(&mut state);
-        aes.mix_columns(&mut state);
-        aes.add_round_key(&mut state, r);
-
-        // Check error
-        let (vec_err, max_err) = get_he_state_error(&he_state, state, &lwe_sk);
-        print!("  - Linear  :");
-        for bit_err in vec_err.iter().take(BYTESIZE * num_bytes_to_print) {
-            print!(" {bit_err:>2}");
-        }
-        println!(" ... (max: {:.3})", (max_err as f64).log2());
-    }
-
-    println!("Final Round");
-    // SubBytes
-    let now = Instant::now();
-    he_sub_bytes_by_pksk(
-        &mut he_state,
-        &ksk,
-        fourier_bsk,
-        &pksk,
-        ss_key,
-        ggsw_base_log,
-        ggsw_level,
-        log_lut_count,
-    );
-    time_sub_bytes += now.elapsed();
-
-    aes.sub_bytes(&mut state);
-    let (vec_err, max_err) = get_he_state_error(&he_state, state, &lwe_sk);
-    print!("  - SubBytes:");
-    for bit_err in vec_err.iter().take(BYTESIZE * num_bytes_to_print) {
-        print!(" {bit_err:>2}");
-    }
-    println!(" ... (max: {:.3})", (max_err as f64).log2());
-
-    let now = Instant::now();
-    // ShiftRows
-    he_shift_rows(&mut he_state);
-
-    // AddRoundKey
-    he_add_round_key(&mut he_state, &he_round_keys[NUM_ROUNDS]);
-    time_linear += now.elapsed();
-
-    aes.shift_rows(&mut state);
-    aes.add_round_key(&mut state, NUM_ROUNDS);
-
-    print!("  - Linear  :");
-    let (vec_err, max_err) = get_he_state_error(&he_state, state, &lwe_sk);
-    for bit_err in vec_err.iter().take(BYTESIZE * num_bytes_to_print) {
-        print!(" {bit_err:>2}");
-    }
-    println!(" ... (max: {:.3})", (max_err as f64).log2());
-
-    // Evaluation Time
-    println!("\n---- Evaluation Time ----");
-    println!("SubBytes: {} s", time_sub_bytes.as_millis() as f64 / 1000f64);
-    println!("Linear  : {} ms", time_linear.as_micros() as f64 / 1000f64);
-
-    let time_total = time_sub_bytes + time_linear;
+    let time_total = time_lwe_ks + time_sub_bytes + time_linear;
     println!("Total   : {} s", time_total.as_millis() as f64 / 1000f64);
 }
